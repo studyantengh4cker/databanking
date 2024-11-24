267172a9a7f4f0a039c3109fcc7a8c3286459ff5
Nathaniel A Lucero <92198311+N1ewb@users.noreply.github.com>
2024-11-24 11:20:46 +0800

    APILA NALANG SAD NI

--- app/(custom_hooks)/useGetCollegeUsers.ts
+++ app/(custom_hooks)/useGetCollegeUsers.ts
@@ -6,19 +6,13 @@ import { useEffect, useState } from "react";
 export const useGetCollegeUsers = (
   p0: string,
   id: string | undefined,
-  { role, college }: { role: string; college: string | null | undefined }
+  { role, college }: { role: string; college: string | undefined }
 ) => {
   const [userData, setUserData] = useState<User[]>([]);
   useEffect(() => {
     const fetchUsers = async () => {
-      //Isa ra ka college and naa so 1 ra
-      if (college && college === "1") {
-        const fetchedUsers = await getCollegeUsers(role, college);
-        setUserData(fetchedUsers);
-      }else {
-        const fetchedUsers = await getCollegeUsers(role);
-        setUserData(fetchedUsers);
-      }
+      const fetchedUsers = await getCollegeUsers(role, college && college);
+      setUserData(fetchedUsers);
     };
 
     fetchUsers();
